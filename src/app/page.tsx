d805e527c24908fb32f68a4b962f4b5d2ad46481
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-04-25 23:31:10 -0700

    feat: home should redirect if fasting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import { UserInfoCard } from "@/components/UserInfoCard";
+import { api } from "@/lib/trpc/server";
+import { redirect } from "next/navigation";
 
 export default async function HomePage() {
+  const currentFast = await api.fast.getCurrentFast({ userId: "1" });
+  if (currentFast) {
+    return redirect("/fast");
+  }
   return (
     <>
       <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
